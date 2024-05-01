8894ac97ce6dd6a7a1b922aba270d761cc3950af
PsammystPrime <samuelprime384@gmail.com>
2024-05-01 16:38:24 +0300

    update Courses.jsx

--- learn/src/Courses.jsx
+++ learn/src/Courses.jsx
@@ -1,10 +1,40 @@
-import { Nav, Footer } from "./App";
-import "./Courses.css";
+import { Footer } from "./App";
+import { Link } from "react-router-dom";
+
+export function Nav() {
+  const name = "Learn";
+
+  return (
+    <div className="nav">
+      <h1>LEEEK</h1>
+      <ul className="navLinks">
+        <Link to="revision">
+          <button>{name}</button>
+        </Link>
+        <Link to="schools">
+          <button>Schools Ranks</button>
+        </Link>
+        <Link to="careers">
+          <button>Careers</button>
+        </Link>
+      </ul>
+    </div>
+  );
+}
+
+export function Levels() {
+  return (
+    <>
+      <h1>Courses</h1>
+    </>
+  );
+}
+
 export default function Courses() {
   return (
     <>
       <Nav />
-      <h1>Courses</h1>
+      <Levels />
       <Footer />
     </>
   );
