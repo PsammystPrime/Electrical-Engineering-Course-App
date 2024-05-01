e65d80a339b5bf9897695c1a68fac358cba34d1b
PsammystPrime <samuelprime384@gmail.com>
2024-05-01 17:49:52 +0300

    create subject routing

--- learn/src/main.jsx
+++ learn/src/main.jsx
@@ -4,12 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import Courses from "./Courses.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Certificate from "./cert.jsx";
+import Certificate, { Certificate1 } from "./cert.jsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "courses", element: <Courses /> },
   { path: "courses/certificate", element: <Certificate /> },
+  { path: "courses/certificate/mod1", element: <Certificate1 /> },
+  { path: "courses/certificate/mod2", element: <Certificate1 /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
