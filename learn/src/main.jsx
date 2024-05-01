d266ecb948f45eb2c0dda3c396e5f4ffdf471415
PsammystPrime <samuelprime384@gmail.com>
2024-05-01 17:56:28 +0300

    update main.jsx

--- learn/src/main.jsx
+++ learn/src/main.jsx
@@ -4,14 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import Courses from "./Courses.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Certificate, { Certificate1 } from "./cert.jsx";
+import Certificate, { Module1 } from "./cert.jsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "courses", element: <Courses /> },
   { path: "courses/certificate", element: <Certificate /> },
-  { path: "courses/certificate/mod1", element: <Certificate1 /> },
-  { path: "courses/certificate/mod2", element: <Certificate1 /> },
+  { path: "courses/certificate/mod1", element: <Module1 /> },
+  { path: "courses/certificate/mod2", element: <Module1 /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
