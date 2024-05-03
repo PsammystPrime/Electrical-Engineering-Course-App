3c0956c9af239e0b429568bd437f5d2d8a2e6b89
PsammystPrime <samuelprime384@gmail.com>
2024-05-03 11:31:17 +0300

    update cert.jsx

--- learn/src/cert.jsx
+++ learn/src/cert.jsx
@@ -1,12 +1,14 @@
 import { Footer } from "./App";
 import { Nav } from "./Courses";
 import { Link } from "react-router-dom";
-import image from "/CRT Worked examples.pdf";
+import imag3 from "/CRT Worked examples.pdf";
 import image2 from "/Analogue Electronics 1, DEE 1 notes_075733 (2).pdf";
+import image from "/KRA.pdf";
 import "./Courses.css";
 import { Document, Page } from "react-pdf";
 import { useState } from "react";
 import { pdfjs } from "react-pdf";
+import { all } from "axios";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.js",
@@ -88,10 +90,12 @@ export function Maths() {
   return (
     <>
       <Nav />
-      <h2>Mathematdics</h2>
-      <Document file={image} onLoadSuccess={loadDocument}>
-        <Page pageNumber={pageNum}></Page>
-      </Document>
+      <h2>Mathematics</h2>
+      <div className="pages">
+        <Document className="doc" file={image} onLoadSuccess={loadDocument}>
+          <Page className="page" pageNumber={pageNum}></Page>
+        </Document>
+      </div>
 
       <Footer />
     </>
